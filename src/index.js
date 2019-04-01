f5ad19d5e4345000b397a21093229cddba77543c
Steve <stephen.davies@digia.com>
2019-04-01 17:44:25 +0300

    adding socket connection

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 const express = require('express');
 const http = require('http');
 const path = require('path');
+const socketio = require('socket.io');
 
 const app = express();
-const server = http.createServer(app)
-
-
+const server = http.createServer(app);
+const io = socketio(server);
 const port = process.env.PORT | 3000;
 
 app.use(express.static(path.join(__dirname, '../public')));
+
+io.on('connection',() => {
+    console.log('New Socket connection!')
+})
 
 server.listen(port, () => {
     console.log('App running on port: ', port)
